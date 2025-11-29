e3892ba5d95b34246259cb16156092c22606ed1d
anshul4141 <anshulprajapati691@gmail.com>
2025-11-29 13:17:27 +0530

    done

---
+++ angular-simple/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  form: any = {
+    data: {},
+    inputError:''
+  }
+
+  constructor(public router: Router) {
+
+  }
+
+  signIn() {
+
+    if (this.form.data.login == 'admin' && this.form.data.password == 'admin') {
+      this.router.navigateByUrl('welcome')
+    } else {
+      this.form.inputError = 'login or password is invalid'
+      this.router.navigateByUrl('login');
+    }
+
+  }
+
+}
---
+++ angular-simple/src/app/login/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  form: any = {
+    data: {}
+  }
+
+  signUp() {
+    console.log('in signUp() function')
+    console.log('firstName==> ', this.form.data.firstName)
+    console.log('lastName===>', this.form.data.lastName)
+    console.log('login===>', this.form.data.login)
+    console.log('password===>', this.form.data.password)
+    console.log('dob===>', this.form.data.dob)
+
+  }
+
+}
