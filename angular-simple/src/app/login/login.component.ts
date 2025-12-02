ad1ded82e9735520f37dfe6bcc3afe5eed06b065
anshul4141 <anshulprajapati691@gmail.com>
2025-12-02 13:20:15 +0530

    done

--- angular-simple/src/app/login/login.component.ts
+++ angular-simple/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +9,48 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  endpoint: string = 'http://localhost:8080/Auth/login'
+
   form: any = {
     data: {},
-    inputError:''
+    inputerror: {}
   }
 
-  constructor(public router: Router) {
+  constructor(public router: Router, public httpService: HttpServiceService) {
 
   }
 
   signIn() {
 
-    if (this.form.data.login == 'admin' && this.form.data.password == 'admin') {
-      this.router.navigateByUrl('welcome')
-    } else {
-      this.form.inputError = 'login or password is invalid'
-      // this.router.navigateByUrl('login');
-    }
+    var self = this;
+
+    // if (this.form.data.login == 'admin' && this.form.data.password == 'admin') {
+    //   this.router.navigateByUrl('welcome')
+    // } else {
+    //   this.form.inputError = 'login or password is invalid'
+    //   // this.router.navigateByUrl('login');
+    // }
+
+    this.httpService.post(this.endpoint, this.form.data, function (res: any) {
+
+      console.log("data == > ", res.result.data);
+      console.log("message == > ", res.result.message)
+      console.log("success ==> ", res.success)
+      console.log("inputerror ==> ", res.result.inputerror)
+
+      if (res.success) {
+        self.router.navigateByUrl('welcome')
+      }
+
+      if (!res.success && res.result.message) {
+        self.form.message = res.result.message
+      }
+
+      if (res.result.inputerror) {
+        self.form.inputerror = res.result.inputerror;
+      }
+
+    })
 
   }
 
--- angular-simple/src/app/login/signup.component.ts
+++ angular-simple/src/app/login/signup.component.ts
@@ -7,25 +7,39 @@ import { HttpServiceService } from '../http-service.service';
 })
 export class SignupComponent {
 
-  constructor(private httpService: HttpServiceService ) { }
+  constructor(private httpService: HttpServiceService) { }
 
   endpoint: string = 'http://localhost:8080/Auth/signUp'
 
   form: any = {
     data: {},
-    message: ''
+    message: '',
+    inputerror: {}
   }
 
   signUp() {
-    console.log('in signUp() function')
-    console.log('firstName==> ', this.form.data.firstName)
-    console.log('lastName===>', this.form.data.lastName)
-    console.log('login===>', this.form.data.loginId)
-    console.log('password===>', this.form.data.password)
-    console.log('dob===>', this.form.data.dob)
+    var self = this
+    // console.log('in signUp() function')
+    // console.log('firstName==> ', this.form.data.firstName)
+    // console.log('lastName===>', this.form.data.lastName)
+    // console.log('login===>', this.form.data.loginId)
+    // console.log('password===>', this.form.data.password)
+    // console.log('dob===>', this.form.data.dob)
 
     this.httpService.post(this.endpoint, this.form.data, function (res: any) {
-      console.log('response from signUp api===>', res)
+      console.log('message===>', res.result.message)
+      console.log('inputerror===>', res.result.inputerror)
+      console.log('Id===>', res.result.data)
+      console.log('status===>', res.success)
+
+      if (res.result.message) {
+        self.form.message = res.result.message;
+      }
+
+      if (res.result.inputerror) {
+        self.form.inputerror = res.result.inputerror;
+      }
+
     })
 
 
