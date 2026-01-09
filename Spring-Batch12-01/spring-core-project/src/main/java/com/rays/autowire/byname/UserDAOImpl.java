41390cdc9d00430a32fa7827e647a4136cf0c3d4
anshul4141 <anshulprajapati691@gmail.com>
2026-01-09 12:58:40 +0530

    done

---
+++ Spring-Batch12-01/spring-core-project/src/main/java/com/rays/autowire/byname/UserDAOImpl.java
@@ -0,0 +1,10 @@
+package com.rays.autowire.byname;
+
+public class UserDAOImpl implements UserDAOInt {
+
+	@Override
+	public void add() {
+		System.out.println("add method..!!");
+	}
+
+}
---
+++ Spring-Batch12-01/spring-core-project/src/main/java/com/rays/autowire/bytype/UserDAOImpl.java
@@ -0,0 +1,10 @@
+package com.rays.autowire.bytype;
+
+public class UserDAOImpl implements UserDAOInt {
+
+	@Override
+	public void add() {
+		System.out.println("add method..!!");
+	}
+
+}
