b83edd5c6eb08ef24d6e595103f1d2f0a745543f
anshul4141 <anshulprajapati691@gmail.com>
2026-02-18 13:09:03 +0530

    done

--- Spring-Batch12-01/angular-simple-project/src/app/app.module.ts
+++ Spring-Batch12-01/angular-simple-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -23,7 +23,8 @@ import { HttpServiceService } from './http-service.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     HttpServiceService
--- Spring-Batch12-01/angular-simple-project/src/app/http-service.service.ts
+++ Spring-Batch12-01/angular-simple-project/src/app/http-service.service.ts
@@ -5,10 +5,18 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpServiceService {
+  constructor(public httpClint: HttpClient) { }
 
-  constructor(public httpClint: HttpClient) {
+  post(endpoint: any, bean: any, callback: any) {
 
-    
+    this.httpClint.post(endpoint, bean).subscribe((response) =>{
+      callback(response);
+    })
 
-   }
+  }
+
+  get(endpoint: any, callback: any) {
+
+  }
+
 }
