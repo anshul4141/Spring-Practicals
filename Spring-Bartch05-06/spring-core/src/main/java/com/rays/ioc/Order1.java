0e6f88c334bdb6d2d8ce762537aaf7a7b24c7733
anshul4141 <anshulprajapati691@gmail.com>
2026-04-03 17:56:21 +0530

    done

---
+++ Spring-Bartch05-06/spring-core/src/main/java/com/rays/ioc/Order1.java
@@ -0,0 +1,42 @@
+package com.rays.ioc;
+
+// by setter injection
+public class Order1 {
+
+	private Inventory inventory;
+	private Payment payment;
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public void bookATicket(int items) {
+
+		int price = 10;
+
+		double totalAmount = items * price;
+
+		double updatedBalance = payment.makePayment(totalAmount);
+
+		int updatedStock = inventory.sold(items);
+
+		System.out.println("Tickets are Booked");
+		System.out.println("Total Amount Paid: " + totalAmount);
+		System.out.println("Remaining Balance: " + updatedBalance);
+		System.out.println("Total Booked Tickects: " + items);
+		System.out.println("Updated Stock: " + updatedStock);
+	}
+
+}
