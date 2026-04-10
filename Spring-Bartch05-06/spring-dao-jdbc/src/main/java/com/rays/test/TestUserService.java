15d13c93f51ec44a075bff511085ed72bfd009ef
anshul4141 <anshulprajapati691@gmail.com>
2026-04-10 17:58:00 +0530

    done

---
+++ Spring-Bartch05-06/spring-dao-jdbc/src/main/java/com/rays/test/TestUserService.java
@@ -0,0 +1,42 @@
+package com.rays.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.rays.dto.UserDTO;
+import com.rays.service.UserService;
+
+@Component("testUserService")
+public class TestUserService {
+
+	@Autowired
+	public UserService service = null;
+
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		TestUserService test = (TestUserService) context.getBean("testUserService");
+
+//		test.testAdd();
+		test.testDelete();
+
+	}
+
+	private void testDelete() {
+		service.delete(3);
+	}
+
+	public void testAdd() {
+		UserDTO dto = new UserDTO();
+		dto.setFirstName("Ram");
+		dto.setLastName("Sharma");
+		dto.setLogin("ram@gmail.com");
+		dto.setPassword("ram123");
+		long i = service.add(dto);
+		System.out.println("Data Inserted... row affected = " + i);
+	}
+
+}
