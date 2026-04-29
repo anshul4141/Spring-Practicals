9d2d9e9881c7ea92174369c35cb8fb7c86712995
anshul4141 <anshulprajapati691@gmail.com>
2026-04-29 17:44:32 +0530

    done

--- Spring-Bartch05-06/spring-boot-simple/src/main/java/com/rays/ctl/RoleCtl.java
+++ Spring-Bartch05-06/spring-boot-simple/src/main/java/com/rays/ctl/RoleCtl.java
@@ -1,6 +1,8 @@
 package com.rays.ctl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +42,62 @@ public class RoleCtl {
 		}
 
 		return res;
+	}
+
+	@PostMapping("update")
+	public ORSResponse update(@RequestBody RoleForm form) {
+
+		ORSResponse res = new ORSResponse();
+
+		RoleDTO dto = new RoleDTO();
+
+		dto.setId(form.getId());
+		dto.setName(form.getName());
+		dto.setDescription(form.getDescription());
+
+		roleService.update(dto);
+
+		res.setSuccess(true);
+		res.addData(dto);
+		res.addMessage("role updated successfulluy");
+
+		return res;
+	}
+
+	@PostMapping("delete/{ids}")
+	public ORSResponse delete(@PathVariable(required = false) long[] ids) {
+
+		ORSResponse res = new ORSResponse();
+
+		if (ids != null && ids.length > 0) {
+			for (long id : ids) {
+				roleService.delete(id);
+				res.addMessage("recored deleted successfully");
+				res.setSuccess(true);
+			}
+		} else {
+			res.addMessage("select at least one record");
+		}
+		return res;
+
+	}
+
+	@GetMapping("get/{id}")
+	public ORSResponse get(@PathVariable(required = false) long id) {
+
+		ORSResponse res = new ORSResponse();
+
+		RoleDTO dto = roleService.findById(id);
+
+		if (dto != null) {
+			res.addData(dto);
+			res.setSuccess(true);
+		} else {
+			res.addMessage("record not found");
+		}
+
+		return res;
+
 	}
 
 }
