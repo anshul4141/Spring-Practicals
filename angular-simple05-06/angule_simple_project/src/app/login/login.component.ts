cee21bb2ec95130f6d233d1731ef891b137da723
anshul4141 <anshulprajapati691@gmail.com>
2026-05-15 17:58:40 +0530

    done

--- angular-simple05-06/angule_simple_project/src/app/login/login.component.ts
+++ angular-simple05-06/angule_simple_project/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(public httpService: HttpServiceService) { }
+
+  endpoint: any = "http://localhost:8081/Auth/login"
+
+  form: any = {
+    data: {},
+    inputerror: {},
+    message: '',
+    errorMessage: ''
+  }
+
+  signIn() {
+    let self = this;
+    this.httpService.post(this.endpoint, this.form.data, (response: any) => {
+      console.log('response === >', response);
+      if (response.success == false && response.result.inputerror) {
+        self.form.inputerror = response.result.inputerror;
+      } else if (response.success == false && response.result.message) {
+        self.form.errorMessage = response.result.message;
+      } else if (response.success == true) {
+        self.form.message = response.result.message;
+      }
+    });
+  }
 
 }
