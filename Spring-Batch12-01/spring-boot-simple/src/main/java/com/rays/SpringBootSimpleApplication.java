9eb05cf1f51e9043d8005ea64039ce4795ec8df1
anshul4141 <anshulprajapati691@gmail.com>
2026-06-05 12:43:40 +0530

    done

--- Spring-Batch12-01/spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
+++ Spring-Batch12-01/spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
@@ -1,15 +1,40 @@
 package com.rays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.rays.common.FrontCtl;
 
 @SpringBootApplication
 public class SpringBootSimpleApplication {
+
+	@Autowired
+	FrontCtl frontCtl;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootSimpleApplication.class, args);
 		System.out.println("spring boot application started at: http://localhost:8080");
+
+	}
+
+	@Bean
+	public WebMvcConfigurer webConfig() {
+
+		WebMvcConfigurer w = new WebMvcConfigurer() {
+
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(frontCtl).addPathPatterns("/**").excludePathPatterns("/Auth/**");
+			}
+
+		};
+
+		return w;
 
 	}
 
--- Spring-Batch12-01/spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
+++ Spring-Batch12-01/spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.rays.common.BaseCtl;
 import com.rays.common.ORSResponse;
 import com.rays.dto.UserDTO;
 import com.rays.form.LoginForm;
+import com.rays.form.UserRegistrationForm;
 import com.rays.service.UserService;
 
 @RestController
@@ -46,6 +48,34 @@ public class LoginCtl extends BaseCtl {
 			res.setSuccess(false);
 			res.addMessage("invalid login or password");
 		}
+
+		return res;
+
+	}
+
+	@PostMapping("signup")
+	public ORSResponse save(@RequestBody @Valid UserRegistrationForm form, BindingResult bindingResult) {
+		ORSResponse res = validate(bindingResult);
+		if (!res.isSuccess()) {
+			return res;
+		}
+		UserDTO dto = (UserDTO) form.getDto();
+
+		long pk = service.add(dto);
+		res.addData(pk);
+		res.addMessage("user register successfully");
+		res.setSuccess(true);
+
+		return res;
+	}
+
+	@GetMapping("logout")
+	public ORSResponse logout(HttpSession session) {
+		ORSResponse res = new ORSResponse();
+		session.invalidate();
+
+		res.setSuccess(true);
+		res.addMessage("user logout successfully");
 
 		return res;
 
