b1b9f9251a8fe51c638c37a36254f3443e223bc2
anshul4141 <anshulprajapati691@gmail.com>
2026-09-25 13:01:05 +0530

    done

---
+++ Spring-Batch12-01/Spring-Dao-Hibernate/src/main/java/com/rays/test/TestUser.java
@@ -0,0 +1,71 @@
+package com.rays.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.rays.dto.UserDTO;
+import com.rays.service.UserService;
+
+@Component("testUser")
+public class TestUser {
+
+	@Autowired
+	UserService service;
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		TestUser test = context.getBean("testUser", TestUser.class);
+
+//		test.testAdd();
+		test.testUpdate();
+//		test.testFindPk();
+//		test.testFindByLogin();
+//		test.testAuthenticate();
+//		test.testSearch();
+
+	}
+
+	/*
+	 * private void testFindPk() {
+	 * 
+	 * UserDTO dto = service.findByPk(1);
+	 * 
+	 * System.out.println(dto.getId()); System.out.println(dto.getFirstName());
+	 * System.out.println(dto.getLastName()); System.out.println(dto.getLogin());
+	 * System.out.println(dto.getPassword());
+	 * 
+	 * }
+	 */
+
+	private void testAdd() {
+
+		UserDTO dto = new UserDTO();
+
+		dto.setFirstName("Harshit");
+		dto.setLastName("Panchal");
+		dto.setLogin("harshit@gmail.com");
+		dto.setPassword("pass123");
+
+		service.save(dto);
+
+	}
+
+	private void testUpdate() {
+
+		UserDTO dto = new UserDTO();
+
+		dto.setId(1);
+		dto.setFirstName("Harshit");
+		dto.setLastName("Sharma");
+		dto.setLogin("harshit@gmail.com");
+		dto.setPassword("pass123");
+
+		service.save(dto);
+
+	}
+
+}
